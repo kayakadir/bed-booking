b3271a17f7e2c2ef9f0565fda822f818eeed861b
Kadir Kaya <kadirkaya@Kadirs-MacBook-Pro.local>
2022-12-19 16:28:54 +0300

    first commit

---
+++ classes.py
@@ -0,0 +1,83 @@
+
+# bedroms has beds
+class Bedroom:
+    def __init__(self, name):
+        self.name = name
+        self.beds = [] # array of Bed objects
+        print(f'bedroom: {name} created!')
+    def addBed(self, b):
+        self.beds.append(b)
+        print('bed should be added')
+    def removeBed(self, b):
+        self.beds.remove(b)
+        print('bed should be removed!')
+    def findEmptyBeds(self): 
+        beds = self.beds
+        filtered_beds = filter(lambda bed: bed.available == True, beds)
+        for bed in filtered_beds:
+            print(bed)
+    def __str__(self):
+        return f'Bedroom; name: {self.name}'
+# application have many beds for guests
+class Bed:
+    def __init__(self, id, type):
+        self.id = id
+        self.type = type
+        self.available = True
+        print(f'Bed id: {id} created!')
+    def reserve(self):
+        self.available = False
+    def release(self):
+        self.available = True
+    def __str__(self):
+        return f'Bed; id: {self.id}, type: {self.type}, available: {self.available}'
+
+class Booking:
+    def __init__(self, bed, startDate, endDate):
+        self.bed = bed
+        self.startDate = startDate
+        self.endDate = endDate
+        self.active = True
+        print(f'Booking created for {bed}')
+    def finishBooking(self):
+        self.active = False
+    def __str__(self) -> str:
+        return f'Booking: {self.bed}, startDate: {self.startDate}, endDate: {self.endDate}, active: {self.active}'
+
+# guest books one or many beds.
+class Guest:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.bookings = []
+        print(f'Guest name: {name} created!')
+    def reserveBed(self, bed, start_date, end_date):
+        # make booking
+        new_book = Booking(bed, start_date, end_date)
+        self.bookings.append(new_book)
+        # make reservation
+        bed.reserve()
+        print('Bed should be reserved!')
+    def releaseBed(self, bed):
+        # make booking deactive
+        book = self.findBooking(bed)
+        if book is not None:
+            book.finishBooking()
+            # make bad available again
+            bed.release()
+        else:
+            print(f'The bed didn\'t find from bookings. So it can\'t released.')
+    def findBooking(self, bed):
+        # searches bed inside of bookings. returns None automaticaly if bed not exist!
+        for booking in self.bookings:
+            if booking.bed == bed:
+                return booking
+    def allBookings(self):
+        # show all bookings of current guest
+        print(f'Bookings of {self.name}')
+        for book in self.bookings:
+            print(book)
+    def __str__(self):
+        return f'Guest; name: {self.name}, email: {self.email}, phone: {self.phone}'
+
---
+++ main.py
@@ -0,0 +1,36 @@
+from classes import Bed, Bedroom, Booking, Guest
+import datetime as dt
+
+daire8 = Bedroom('daire8')
+
+b1 = Bed(1, 'single')
+b2 = Bed(2, 'single')
+b3 = Bed(3, 'single')
+daire8.findEmptyBeds()
+daire8.addBed(b1)
+daire8.addBed(b2)
+daire8.addBed(b3)
+""" daire8.findEmptyBeds()
+daire8.removeBed(b1)
+daire8.findEmptyBeds()
+daire8.addBed(b1)
+daire8.findEmptyBeds()
+
+ """
+guest1 = Guest('Kadir', 'kadir@gmail.com', '+905555555555')
+guest2 = Guest('Ahmet', 'ahmet@gmail.com', '+905555555555')
+guest3 = Guest('Mehmet', 'mehmet@gmail.com', '+905555555555')
+
+# reserve bed
+print('reserving bed')
+s_date = dt.datetime.now()
+e_date = dt.datetime(2022, 12, 30)
+guest1.reserveBed(b1, s_date, e_date)
+daire8.findEmptyBeds()
+guest1.allBookings()
+
+# release bed
+print('releasing bed')
+guest1.releaseBed(b1)
+daire8.findEmptyBeds()
+guest1.allBookings()
